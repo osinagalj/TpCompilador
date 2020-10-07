6f3ba344b92a2b8eec4d572b003abdd4f1b471c4
osinagalj <losinaga@alumnos.exa.unicen.edu.ar>
2020-10-07 16:54:45 -0300

    eng 3.0

--- Compilador/Output/Logger.h
+++ Compilador/Output/Logger.h
@@ -1,10 +1,7 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-
 #include "../Lexical_analyzer/Lexical_analyzer.h"
-
-
 using namespace std;
 
 class Logger{
@@ -13,11 +10,10 @@ public:
     static void open( const string & logFile);
     static void close();
     static void write( const string & message);
-    //static void lex(const Lexical_analyzer * lexico);
     static Lexical_analyzer * lexico;
     Logger();
 private:
-    //Lexi
+
     ofstream fileStream;
     //Logger instance (singleton)
     static Logger instance;
