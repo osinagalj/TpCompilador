cd3320625601e6fbaf43522864ab806923d646c0
osinagalj <49956395+osinagalj@users.noreply.github.com>
2020-11-23 12:52:54 -0300

    update invoke gramatica

--- Compilador/GeneracionDeCodigoAssebler/out_assembler.asm
+++ Compilador/GeneracionDeCodigoAssebler/out_assembler.asm
@@ -23,23 +23,28 @@ includelib \masm32\lib\user32.lib
     c@main DD ?
     d@main DD ?
     e@main@procedure_1 DD ?
+    e@main@procedure_1@procedure_3 DD ?
     e@main@procedure_3 DD ?
     float_negative@main DD ? 
+    hola@main DD ?
     longint_negative@main DD ?
     p1@main@procedure_1 DD ?
+    p1@main@procedure_1@procedure_3 DD ?
     variable_aux@main@procedure_3@WHILE_1@IF_2 DD ?
     w@main DD ? 
     x@main DD ? 
     y@main DD ? 
     z@main DD ? 
-    @aux1 DD 5000.000000
-    @aux2 DD 3.300000
+    @aux1 DD 1.000000
+    @aux2 DD ? 
     @aux3 DD 5000.000000
-    @aux4 DD ? 
-    @aux5 DD 4.500000
-    @aux6 DD 4.200000
-    @aux7 DD ? 
-    @aux8 DD -7.300000
+    @aux4 DD 3.300000
+    @aux5 DD 5000.000000
+    @aux6 DD ? 
+    @aux7 DD 4.500000
+    @aux8 DD 4.200000
+    @aux9 DD ? 
+    @aux10 DD -7.300000
     str10 DB "Error: Se realizo 2 una division por 0" , 0 
 .CODE
     procedure_1@main PROC
@@ -61,12 +66,18 @@ includelib \masm32\lib\user32.lib
         SUB ECX,-3
         MOV d@main,ECX
         ret
-    procedure_1@main ENDP
-    procedure_3@main:
-        MOV ECX,e@main@procedure_3
+        procedure_1@main  ENDP
+    procedure_3@main@procedure_1 PROC
+        MOV ECX,5
+        ADD ECX,7
+        MOV e@main@procedure_1@procedure_3,ECX
+        ret
+        procedure_3@main@procedure_1  ENDP
+    procedure_3@main PROC
         MOV ECX,2
-        Label33:
-        JG Label51
+        MOV e@main@procedure_3,ECX
+        Label37:
+        JG Label55
         MOV ECX,e@main@procedure_3
         ADD ECX,8
         MOV e@main@procedure_3,ECX
@@ -74,50 +85,54 @@ includelib \masm32\lib\user32.lib
         ADD ECX,3
         MOV variable_aux@main@procedure_3@WHILE_1@IF_2,ECX
         invoke MessageBox, NULL, addr str2, addr str2, MB_OK
-        MOV ECX,e@main@procedure_3
         MOV ECX,12
-        JMP Label49
-        Label45:
-        MOV ECX,longint_negative@main
-        SUB ECX,1
-        MOV longint_negative@main,ECX
-        invoke MessageBox, NULL, addr str1, addr str1, MB_OK
+        MOV e@main@procedure_3,ECX
+        JMP Label53
         Label49:
-        JMP Label33
-        Label51:
+        FLD @aux1
+        FLD float_negative@main
+        FADD 
+        FSTP @aux2
+        FLD @aux2
+        FSTP float_negative@main
+        invoke MessageBox, NULL, addr str1, addr str1, MB_OK
+        Label53:
+        JMP Label37
+        Label55:
         ret
+        procedure_3@main ENDP 
 START:
     invoke MessageBox, NULL, addr str6, addr str6, MB_OK
-    MOV ECX,a@main
     MOV ECX,8
-    FLD @aux1
-    FSTP x@main
-    FLD @aux2
+    MOV a@main,ECX
     FLD @aux3
-    FADD 
-    FSTP @aux4
+    FSTP x@main
     FLD @aux4
+    FLD @aux5
+    FADD 
+    FSTP @aux6
+    FLD @aux6
     FSTP w@main
     MOV EAX , 2
     MOV ECX,3
     MUL ECX
     MOV ECX,EAX
     MOV a@main,ECX
-    MOV ECX,c@main
     MOV ECX,5
+    MOV c@main,ECX
     MOV ECX,a@main
     SUB ECX,2
     MOV b@main,ECX
-    FLD @aux5
-    FLD @aux6
-    FADD 
-    FSTP @aux7
     FLD @aux7
-    FSTP w@main
     FLD @aux8
+    FADD 
+    FSTP @aux9
+    FLD @aux9
+    FSTP w@main
+    FLD @aux10
     FSTP float_negative@main
-    MOV ECX,longint_negative@main
     MOV ECX,-715
+    MOV longint_negative@main,ECX
     invoke MessageBox, NULL, addr str7, addr str7, MB_OK
     invoke MessageBox, NULL, addr str5, addr str5, MB_OK
     CALL procedure_1@main
